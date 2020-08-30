54eb2eabe17f9edd3ded3b79a290cccc8404e939
Mjanaina <mariajanainadasilva567@gmail.com>
2020-08-30 16:31:53 -0300

    conexão com o banco

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuAu6_jifj9Uy9UNfc06jno3yPRGFdRYI",
+    authDomain: "app-schooldiary.firebaseapp.com",
+    databaseURL: "https://app-schooldiary.firebaseio.com",
+    projectId: "app-schooldiary",
+    storageBucket: "app-schooldiary.appspot.com",
+    messagingSenderId: "498957271416",
+    appId: "1:498957271416:web:c64082a33c1a5e3696088d",
+    measurementId: "G-W1GYPL4H70"
+}
+
+export default firebaseConfig
