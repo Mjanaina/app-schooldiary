cda59fc33415fd5d2a9e0055f9095e79b94f82bd
mjanaina <mariajanainadasilva567@gmail.com>
2020-09-05 00:01:23 -0300

    adicionando eventos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,13 +7,45 @@ import { CalendarComponent } from 'ionic2-calendar';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  currentDAte = new Date();
+
+  Evento = {
+    titulo: '',
+    descricao: '',
+    comeco: '',
+    fim: ''
+  } 
+  allEvents = [];
+  minDate = new Date().toISOString();
+  currentDate = new Date();
   currentMonth: string;
   @ViewChild(CalendarComponent, {static: false}) myCalendar: CalendarComponent;
+  showAddEvent: boolean;
 
   constructor() {}
 
   onViewTitleChanged(title: string) {
     this.currentMonth = title;
   }
+
+  showHideForm() {
+    this.showAddEvent = !this.showAddEvent;
+    this.Evento = {
+    titulo: '',
+    descricao: '',
+    comeco: new Date().toISOString(),
+    fim: new Date().toISOString()
+  };
 }
+
+addEvento() {
+  this.allEvents.push({
+    title: this.Evento.titulo,
+    startTime:  new Date(this.Evento.comeco),
+    endTime: new Date(this.Evento.fim),
+    description: this.Evento.descricao
+    });
+  this.showHideForm();
+}
+  }
+
+  
